d7c38b21b0d38fdc7900e0a1abb1302f1c6f0e93
James Mburu Githire <jamesmburug24@gmail.com>
2022-09-16 07:44:11 +0300

    removed functionality, recieved props

--- src/Image.js
+++ src/Image.js
@@ -1,35 +1,16 @@
 import { Component } from "react";
-
-function fetcher() {
-    return fetch("https://dog.ceo/api/breeds/image/random");
-}
-
 export default class ImageDiv extends Component {
     constructor(props) {
         super();
-        console.log(props);
     }
-    state = {}
-    componentWillMount() {
-        fetcher()
-            .then(resp => resp.json())
-            .then(result => this.setState(result))
-    }
-
     render() {
-        let imageFetcher = () => {
-            fetcher()
-                .then(resp => resp.json())
-                .then(result => this.setState(result))
-        }
-        console.log(this.state)
         return (
             <div className='ImageDiv'>
                 <div className="Image-title"><small>Flatagram Random Dog</small></div>
                 <div
-                    onClick={imageFetcher}
+                    onClick={this.props.handleClicks}
                     className="ImageTile"
-                    style={{ backgroundImage: `url(${this.state.message})` }}></div>
+                    style={{ backgroundImage: `url(${this.props.newImage})` }}></div>
             </div>
         );
     }
