6cb3639e6e5498ee9864b01e04d1896f06510527
James Mburu Githire <jamesmburug24@gmail.com>
2022-09-16 06:11:08 +0300

    Image apears and changes onclick

--- src/Image.js
+++ src/Image.js
@@ -3,24 +3,33 @@ import { Component } from "react";
 function fetcher() {
     return fetch("https://dog.ceo/api/breeds/image/random");
 }
+
 export default class ImageDiv extends Component {
-    constructor() {
+    constructor(props) {
         super();
+        console.log(props);
     }
     state = {}
-    componentWillMount(){
+    componentWillMount() {
         fetcher()
             .then(resp => resp.json())
             .then(result => this.setState(result))
     }
+
     render() {
+        let imageFetcher = () => {
+            fetcher()
+                .then(resp => resp.json())
+                .then(result => this.setState(result))
+        }
         console.log(this.state)
         return (
             <div className='ImageDiv'>
                 <div className="Image-title"><small>Flatagram Random Dog</small></div>
-                <div 
-                className ="ImageTile" 
-                style={{backgroundImage:this.state.message}}></div>
+                <div
+                    onClick={imageFetcher}
+                    className="ImageTile"
+                    style={{ backgroundImage: `url(${this.state.message})` }}></div>
             </div>
         );
     }
