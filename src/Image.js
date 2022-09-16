1b963d741ed63dc612ff2f1277cdc07a3640c13d
James Mburu Githire <jamesmburug24@gmail.com>
2022-09-16 05:59:35 +0300

    setState in componentWillMount

--- src/Image.js
+++ src/Image.js
@@ -1,12 +1,26 @@
 import { Component } from "react";
 
+function fetcher() {
+    return fetch("https://dog.ceo/api/breeds/image/random");
+}
 export default class ImageDiv extends Component {
+    constructor() {
+        super();
+    }
+    state = {}
+    componentWillMount(){
+        fetcher()
+            .then(resp => resp.json())
+            .then(result => this.setState(result))
+    }
     render() {
-
+        console.log(this.state)
         return (
             <div className='ImageDiv'>
                 <div className="Image-title"><small>Flatagram Random Dog</small></div>
-                <img src="./flatagram-logo.png" />
+                <div 
+                className ="ImageTile" 
+                style={{backgroundImage:this.state.message}}></div>
             </div>
         );
     }
