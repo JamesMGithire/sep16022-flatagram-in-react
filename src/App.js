3f2be104bab6f017cf469adf4065800f624a95a9
James Mburu Githire <jamesmburug24@gmail.com>
2022-09-16 07:43:37 +0300

    Brought functionality from Image.js, used props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import ImageDiv from './Image';
 
-function App() {
+export default function App() {
+  const [states, setStates] = React.useState({});
+  function fetcher() {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(resp => resp.json())
+      .then(result => setStates(result))
+  }
+  useEffect(() => {
+    fetcher()
+  }, [])
   return (
     <div className="App">
       <img className="logo" src='./flatagram-logo.png' />
       <div className='Flatagram-container'>
-        <ImageDiv/>
+        <ImageDiv
+          handleClicks={fetcher}
+          newImage={states.message}
+        />
       </div>
     </div>
   );
 }
-
-export default App;
