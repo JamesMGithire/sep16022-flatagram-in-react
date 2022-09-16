b9dfe72e26ec5784f93ac33b48a834927e120f72
James Mburu Githire <jamesmburug24@gmail.com>
2022-09-16 09:42:12 +0300

    Display dog breed as title

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import './App.css';
 import ImageDiv from './Image';
 
 export default function App() {
-  const [states, setStates] = React.useState({});
+  const [states, setStates] = React.useState("");
   function fetcher() {
     fetch("https://dog.ceo/api/breeds/image/random")
       .then(resp => resp.json())
-      .then(result => setStates(result))
+      .then(result => setStates(result.message))
   }
   useEffect(() => {
     fetcher()
   }, [])
+  let title = states
+  let newTtl = (title.replace("https://images.dog.ceo/breeds/", "").slice(0, title.indexOf("/"))).replace("/", "")
+  let capitalisedTitle = newTtl.charAt(0).toUpperCase() + newTtl.slice(1)
   return (
     <div className="App">
-      <img className="logo" src='./flatagram-logo.png' />
+      <img className="logo" src='./flatagram-logo.png' alt='flatagram logo' />
       <div className='Flatagram-container'>
         <ImageDiv
           handleClicks={fetcher}
-          newImage={states.message}
+          newImage={states}
+          newTitle={capitalisedTitle}
         />
       </div>
     </div>
