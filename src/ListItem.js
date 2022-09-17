2ff883ff5ff81ce7141a5f5a4d0eb9e70595b869
James Mburu Githire <jamesmburug24@gmail.com>
2022-09-17 09:35:07 +0300

    Added id from id in props

--- src/ListItem.js
+++ src/ListItem.js
@@ -3,7 +3,7 @@ import { Component } from "react"
 export default class ListItem extends Component {
     render() {
         return (
-            <li
+            <li id={this.props.id}
                 onClick={this.props.del}
             >
                 {this.props.content}
